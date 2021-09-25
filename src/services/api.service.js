866774cd896eb04d1f8ddd1acbfaa907ae73f79b
Dvornick <mail@dvornick.ru>
2021-09-25 23:07:28 +0300

    Receiving data from the server

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -11,13 +11,25 @@ class ApiService {
                 method: 'post',
                 body: JSON.stringify(post)
             })
-            const response = await fetch(request)
-            return await response.json()
+            return useRequest(request)
         } catch (e) {
             console.error(e)
         }
-
     }
+
+    async fetchPosts() {
+        try {
+            const request = new Request(this.url + 'post.json')
+            return useRequest(request)
+        } catch (e) {
+            console.error(e)
+        }
+    }
+}
+
+async function useRequest(request) {
+    const response = await fetch(request)
+    return await response.json()
 }
 
 export const apiService = new ApiService(config['default-rtdb'])
